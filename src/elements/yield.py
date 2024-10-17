480775b06bb6546f1f89a691e66682467b96250a
Mercedes Roman Ruiz <mercedesromanruiz2002@gmail.com>
2024-10-17 12:07:19 +0200

    Assigment 3

--- src/elements/yield.py
+++ src/elements/yield.py
@@ -21,10 +21,12 @@ def equivalentStress(criterion, stress):
     c = criterion.lower()
 
     if (c == "vonmises"):
-        eq = 0.0
+        eq = (1/2 * ((stress[0, 0] - stress[1, 1])**2 + (stress[1, 1] - stress[2, 2])**2 + (stress[2, 2] - stress[0, 0])**2) + 3 * (stress[0, 1]**2 + stress[1, 2]**2 + stress[2, 0]**2))**(1/2)
 
     elif (c == "tresca"):
-        eq = 0.0
+        eigenvalues = np.linalg.eigvals(stress)
+        # print("Eigenvalues: ", eigenvalues)
+        eq = np.max(eigenvalues) - np.min(eigenvalues)
 
     else:
         eq = 0.0
@@ -50,18 +52,18 @@ def testStress():
     stress[0,0] = s
     print("\n2. Pure compression stress tensor:\n", stress)
     print("\n -- von Mises equivalent stress: ", equivalentStress("vonmises", stress))
-    print(" -- von Mises error: ", s - equivalentStress("vonmises", stress))
+    print(" -- von Mises error: ", s + equivalentStress("vonmises", stress))
     print(" -- Tresca equivalent stress", equivalentStress("tresca", stress))
-    print(" -- Tresca error: ", s - equivalentStress("tresca", stress))
+    print(" -- Tresca error: ", s + equivalentStress("tresca", stress))
 
     stress = np.zeros((3,3))
     s = abs(random.random())
     stress[0,1] = s
     stress[1,0] = s
-    print("\n3. Pure traction stress tensor:\n", stress)
+    print("\n3. Pure shear stress tensor:\n", stress)
     print("\n -- von Mises equivalent stress", equivalentStress("vonmises", stress))
     print(" -- Tresca equivalent stress", equivalentStress("tresca", stress))
-    print(" -- Tresca error: ", s - equivalentStress("tresca", stress))
+    print(" -- Tresca error: ", 2*s - equivalentStress("tresca", stress))
 
     m = np.random.rand(3, 3)
     stress = 0.5*(m+m.T)
